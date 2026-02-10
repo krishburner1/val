01d6a5742c799ea2829f45e3bd271cb12266e7f2
krishburner1 <krisharma1501@gmail.com>
2026-02-10 16:01:55 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+const noBtn = document.getElementById("no");
+const yesBtn = document.getElementById("yes");
+const celebration = document.getElementById("celebration");
+const options = document.querySelector(".options");
+
+yesBtn.addEventListener("click", () => {
+    celebration.classList.remove("hidden");
+
+    for(let i = 0; i<20; i++){
+        const confetti = document.createElement("div");
+        confetti.classList.add("confetti");
+        confetti.style.left = Math.random() * 100 + "%";
+        confetti.style.top = "0";
+        confetti.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 60%)`;
+        document.querySelector(".card").appendChild(confetti);
+
+        setTimeout(() => confetti.remove(), 1500);
+    }
+});
+
+let isMoving = false;
+
+document.addEventListener("mousemove", (e) => {
+    if (isMoving) return;
+
+    const btnRect = noBtn.getBoundingClientRect();
+    const btnCenterX = btnRect.left + btnRect.width / 2;
+    const btnCenterY = btnRect.top + btnRect.height / 2;
+
+    const distance = Math.hypot(
+        e.clientX - btnCenterX,
+        e.clientY - btnCenterY
+    );
+
+    if (distance < 90) {
+        moveNoButton();
+    }
+});
+
+function moveNoButton() {
+    isMoving = true;
+
+    const maxX = options.clientWidth - noBtn.offsetWidth;
+    const maxY = options.clientHeight - noBtn.offsetHeight;
+
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
+
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+
+    // cooldown — prevents vibration
+    setTimeout(() => {
+        isMoving = false;
+    }, 250); // match CSS transition time
+}
